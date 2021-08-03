033c317637e46a4f4ba0d4c2ee4a38e35c901c07
Caio Garcia <caio.augusto@algar.com.br>
2021-08-03 20:24:41 -0300

    End of chapter 1.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,29 @@
 fun main(args: Array<String>) {
-    var x = 1
+
+
+
+    //YabbaDabbaDo (pg 81)
+    /*var x = 1
+
+    while(x < 3) {
+        print(if (x == 1) "Yab" else "Dab")
+        print("ba")
+        x += 1
+    }
+
+    if(x == 3) println("Do")*/
+
+
+    //Using if() as an expression (pg 79)
+    /*val x = 3
+    val y = 1
+
+    println(if(x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what.")*/
+
+
+    //Basic while() implementation (pg 74)
+    /*var x = 1
     println("Before the loop x = $x.")
 
     while(x < 4) {
@@ -7,5 +31,9 @@ fun main(args: Array<String>) {
         x += 1
     }
 
-    println("After the loop. x = $x.")
+    println("After the loop. x = $x.")*/
+
+
+    //First commit (pg 67)
+    //println("Pow!")
 }
